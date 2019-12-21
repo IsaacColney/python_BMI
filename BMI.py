6381b49ae2d2d7bfb352cd0b8e1164365e01846d
Kaazi <47986340+IsaacColney@users.noreply.github.com>
2019-12-21 23:06:13 +0530

    Update and rename BMI.py to bmi_calculator.py

--- BMI.py
+++
@@ -1,9 +0,0 @@
-#BMI Calculator
-w = input("Enter your weigth (kg)")
-h = input("Enter your Height (m)")
-
-def bmi():
-    a = float(w)/(float(h)*float(h))
-    return a
-    
-print ("Your BMI is :", bmi())
---
+++ bmi_calculator.py
@@ -0,0 +1,19 @@
+#BMI Calculator
+#Author Isaac
+def bmi():
+    a = float(w)/(float(h)*float(h))
+    return a
+    
+again = "y"
+
+while again.lower() == "y":
+    w = input("Enter your weigth (kg)")
+    h = input("Enter your Height (m)")
+
+
+    
+    print ("Your BMI is :", bmi())
+
+    again = input("Do you want to Calculate again?y/n:")
+    if again == "n":
+        break
